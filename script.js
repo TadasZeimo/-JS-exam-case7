7ce3f8182995d238b12fa79f366bd79d0284303d
Tadas <tadas.zeimo@gmail.com>
2022-10-27 19:52:55 +0300

    case7 solved

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-	make: "audi",
-	model: "A6",
-	year: 2005,
-	color: "white",
+  make: "audi",
+  model: "A6",
+  year: 2005,
+  color: "white",
 };
+
+function showObjectKeys(object) {
+  return Object.keys(object);
+}
+
+console.log(showObjectKeys(audi));
